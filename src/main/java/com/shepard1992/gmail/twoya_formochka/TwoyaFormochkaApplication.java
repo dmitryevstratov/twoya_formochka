4702cfd2d8094e4a6505cc7a842b16cad7c83f51
dmitryevstratov <evstratovdmitry1992@gmail.com>
2022-03-26 20:31:44 +0300

    create project

---
+++ src/main/java/com/shepard1992/gmail/twoya_formochka/TwoyaFormochkaApplication.java
@@ -0,0 +1,13 @@
+package com.shepard1992.gmail.twoya_formochka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwoyaFormochkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwoyaFormochkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shepard1992/gmail/twoya_formochka/TwoyaFormochkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shepard1992.gmail.twoya_formochka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoyaFormochkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
