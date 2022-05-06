1a8aa60bc405a1f3d62b26d4bb7e6482e8a2b28e
dmitryevstratov <evstratovdmitry1992@gmail.com>
2022-05-06 16:49:32 +0300

    yf-019. service order. delete

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -10,29 +10,35 @@ function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function addItemToTable(item, tableItems, tmp, suffix) {
+function getIndexForItem(tableItems) {
     let index = getMaxIndex(tableItems.querySelectorAll(TAG_TR));
     index = Number(index) + Number(1);
+    return index;
+}
+
+function addItemToTable(item, tableItems, tmp, suffix) {
+    let index = getIndexForItem(tableItems);
     tmp += createItemRowForOrder(item, suffix, index);
     tableItems.innerHTML = tmp;
-    setTotalForItems(suffix);
+    setTotalForUpdateItems(suffix);
 }
 
 function addItemToTableUpdate(item, tableItems, tmp, suffix) {
-    let index = getMaxIndex(tableItems.querySelectorAll(TAG_TR));
-    index = Number(index) + Number(1);
+    let index = getIndexForItem(tableItems);
     tmp += createItemRowForOrder(item, suffix, index);
     tableItems.innerHTML += tmp;
-    setTotalForItems(suffix);
+    setTotalForUpdateItems(suffix);
+}
+
+function addItemToTableDelete(item, tableItems, tmp, suffix) {
+    let index = getIndexForItem(tableItems);
+    tmp += createItemRowForDeleteOrder(item, suffix, index);
+    tableItems.innerHTML += tmp;
+    setTotalForDeleteItems(suffix);
 }
 
 function createItemRowForOrder(item, suffix, index) {
-    let row;
-    row = "<tr id=" + ITEM + index + ">" + "<td class=" + ITEM_ARTICLE_CL + ">" + item.id + "</td>";
-    row += "<td>" + item.name + "</td>";
-    row += "<td>" + item.category.name + "</td>";
-    row += "<td>" + item.type.name + "</td>";
-    row += "<td>" + item.size + "</td>";
+    let row = getRowForTableItems(index, item);
     row += "<td>" + "<button type='button' onclick=incrementItem(" +
         index + ',' + item.price + ',' + "'" + suffix + "'" +
         ")>+</button><span class= " + ITEM_COUNT_CL + " id=" + ITEM_COUNT + index + ">1</span><button type='button' onclick=decrementItem(" +
@@ -43,6 +49,24 @@ function createItemRowForOrder(item, suffix, index) {
         ")\" type=\"button\">\n" +
         " Удалить" +
         "</button>" + "</td></tr>";
+
+    return row;
+}
+
+function getRowForTableItems(index, item) {
+    let row;
+    row = "<tr id=" + ITEM + index + ">" + "<td class=" + ITEM_ARTICLE_CL + ">" + item.id + "</td>";
+    row += "<td>" + item.name + "</td>";
+    row += "<td>" + item.category.name + "</td>";
+    row += "<td>" + item.type.name + "</td>";
+    row += "<td>" + item.size + "</td>";
+    return row;
+}
+
+function createItemRowForDeleteOrder(item, suffix, index) {
+    let row = getRowForTableItems(index, item);
+    row += "<td><span class= " + ITEM_COUNT_CL + " id=" + ITEM_COUNT + index + ">1</span></td>";
+    row += "<td class=" + ITEM_PRICE_CL + " id=" + ITEM_PRICE + index + ">" + item.price + "</td></tr>";
 
     return row;
 }
